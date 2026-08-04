81035d2d73d309ccbdb29fb99184ef6b4a3ae31b
Supakit Wiwustrinon (STM) <supakit_w@siamtoyota.co.th>
2026-08-04 13:04:33 +0700

    fix

--- scripts/download.sh
+++ scripts/download.sh
@@ -5,14 +5,14 @@ echo "Start download system"
 echo "Host: $SFTP_HOST"
 echo "Path: $SFTP_PATH"
 
-mkdir -p ./data
+mkdir -p /data
 
 sshpass -p "$SFTP_PASSWORD" \
   sftp \
   -o StrictHostKeyChecking=no \
   "$SFTP_USERNAME@$SFTP_HOST" <<EOF
 cd $SFTP_PATH
-lcd ./data
+lcd /data
 mget *.xlsx
 bye
 EOF
